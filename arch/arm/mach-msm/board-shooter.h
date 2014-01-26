4f3a88e13196a8bbe1da426cac30b33d5a618fdd
Darescu Ionut <ionut.darescu@gmail.com>
2014-01-26 02:22:51 +0200

    shooter: More memory remap
    10x sultanxda

--- arch/arm/mach-msm/board-shooter.h
+++ arch/arm/mach-msm/board-shooter.h
@@ -1,4 +1,4 @@
-/* linux/arch/arm/mach-msm/board-spade.h
+/* linux/arch/arm/mach-msm/board-shooter.h
  *
  * Copyright (C) 2010-2011 HTC Corporation.
  *
@@ -59,27 +59,26 @@
 /* Note: must be a multiple of 4096 */
 #define MSM_FB_SIZE roundup((960 * ALIGN(540, 32) * 4 * 3) + 0x3F4800, 4096)
 
-/* PMEM Memory map  */
+// PMEM
 #define MSM_PMEM_ADSP_SIZE      0x1200000 /* 18MB */
 #define MSM_PMEM_AUDIO_SIZE     0x239000 /* 2.22MB  */
 
-#define MSM_PMEM_AUDIO_BASE     0x46400000
-#define MSM_PMEM_ADSP_BASE      0x7EE00000
-/* End PMEM */
-
-/* ION Memory map */
 #define MSM_ION_HEAP_NUM        5
 
-#define MSM_ION_MM_SIZE         0x3500000
+// ION
+#define MSM_ION_MM_SIZE         0x2A00000
 #define MSM_ION_MM_FW_SIZE      0x200000
 #define MSM_ION_SF_SIZE         0x29A0000
-#define MSM_ION_WB_SIZE         0x800000
+#define MSM_ION_WB_SIZE         0x2FD000
 
-#define MSM_ION_MM_FW_BASE      0x40400000
-#define MSM_ION_MM_BASE         0x40B00000
-#define MSM_ION_WB_BASE         0x46100000
-#define MSM_ION_SF_BASE         0x7C600000
-/* End ION */
+// Memory bases
+#define MSM_ION_SF_BASE         0x40400000
+#define MSM_ION_WB_BASE         0x45C00000
+#define MSM_PMEM_AUDIO_BASE     0x46400000
+#define MSM_ION_MM_FW_BASE      0x7C200000
+#define MSM_ION_MM_BASE         0x7C400000
+#define MSM_PMEM_ADSP_BASE      0x7EE00000
+
 
 #define MSM_SMI_BASE		0x38000000
 #define MSM_SMI_SIZE		0x4000000
@@ -104,7 +103,7 @@
 #else
 #define PHY_BASE_ADDR1          0x48000000
 #endif
-#define SIZE_ADDR1              0x34000000
+#define SIZE_ADDR1              0x34200000
 
 /* GPIO definition */
 
