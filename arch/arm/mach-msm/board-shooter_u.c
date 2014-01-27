6a9cd04c08cc5e59f2c6e34315e91233e7581aa3
Darescu Ionut <ionut.darescu@gmail.com>
2014-01-28 00:10:40 +0200

    Revert "msm_fb: fbram: Use newer method of allocating RAM to fb"
    credit: sultanxda

--- arch/arm/mach-msm/board-shooter_u.c
+++ arch/arm/mach-msm/board-shooter_u.c
@@ -2323,15 +2323,14 @@ static struct platform_device ion_dev = {
 
 static void __init msm8x60_allocate_memory_regions(void)
 {
-        void *addr;
 	unsigned long size;
 
 	size = MSM_FB_SIZE;
-        addr = alloc_bootmem_align(size, 0x1000);
-        msm_fb_resources[0].start = __pa(addr);
+  msm_fb_resources[0].start = MSM_FB_BASE;
 	msm_fb_resources[0].end = msm_fb_resources[0].start + size - 1;
-        pr_info("allocating %lu bytes at %p (%lx physical) for fb\n",
-                size, addr, __pa(addr));
+  pr_info("allocating %lu bytes at 0x%p (0x%lx physical) for fb\n",
+    size, __va(MSM_FB_BASE), (unsigned long) MSM_FB_BASE);
+
 #ifdef CONFIG_FB_MSM_OVERLAY_WRITEBACK
 	size = MSM_OVERLAY_BLT_SIZE;
 	msm_fb_resources[1].start = MSM_OVERLAY_BLT_SIZE;
