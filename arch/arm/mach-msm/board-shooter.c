57788b04ea3e2518d619d0aeeb1d4b27c526bc5a
Darescu Ionut <ionut.darescu@gmail.com>
2014-03-05 00:40:00 +0200

    shooter: Build new board-display
    *Using new mdp drivers
    
    credit:sultanxda

--- arch/arm/mach-msm/board-shooter.c
+++ arch/arm/mach-msm/board-shooter.c
@@ -146,8 +146,6 @@
 static struct platform_device ion_dev;
 #endif
 
-int __init pyd_init_panel(struct resource *res, size_t size);
-
 enum {
 	GPIO_EXPANDER_IRQ_BASE  = PM8901_IRQ_BASE + NR_PMIC8901_IRQS,
 	GPIO_EXPANDER_GPIO_BASE = PM8901_GPIO_BASE + PM8901_MPPS,
@@ -520,6 +518,9 @@ static struct platform_device smsc911x_device = {
 		defined(CONFIG_CRYPTO_DEV_QCRYPTO_MODULE) || \
 		defined(CONFIG_CRYPTO_DEV_QCEDEV) || \
 		defined(CONFIG_CRYPTO_DEV_QCEDEV_MODULE)
+
+#define QCE_SIZE		0x10000
+#define QCE_0_BASE		0x18500000
 
 #define QCE_HW_KEY_SUPPORT	0
 #define QCE_SHA_HMAC_SUPPORT	0
@@ -2022,14 +2023,6 @@ static struct platform_device msm_batt_device = {
 };
 #endif
 
-static unsigned fb_size;
-static int __init fb_size_setup(char *p)
-{
-	fb_size = memparse(p, NULL);
-	return 0;
-}
-early_param("fb_size", fb_size_setup);
-
 #ifdef CONFIG_ANDROID_PMEM
 static unsigned pmem_adsp_size = MSM_PMEM_ADSP_SIZE;
 
@@ -2049,16 +2042,6 @@ static int __init pmem_audio_size_setup(char *p)
 }
 early_param("pmem_audio_size", pmem_audio_size_setup);
 #endif
-
-static struct resource msm_fb_resources[] = {
-	{
-		.flags  = IORESOURCE_DMA,
-	},
-	/* for overlay write back operation */
-	{
-		.flags  = IORESOURCE_DMA,
-	},
-};
 
 static struct platform_device shooter_3Dpanel_device = {
 	.name = "panel_3d",
@@ -2333,21 +2316,8 @@ static struct platform_device ion_dev = {
 
 static void __init msm8x60_allocate_memory_regions(void)
 {
-	unsigned long size;
+	shooter_allocate_fb_region();
 
-	size = MSM_FB_SIZE;
-  msm_fb_resources[0].start = MSM_FB_BASE;
-	msm_fb_resources[0].end = msm_fb_resources[0].start + size - 1;
-  pr_info("allocating %lu bytes at 0x%p (0x%lx physical) for fb\n",
-    size, __va(MSM_FB_BASE), (unsigned long) MSM_FB_BASE);
-
-#ifdef CONFIG_FB_MSM_OVERLAY_WRITEBACK
-	size = MSM_OVERLAY_BLT_SIZE;
-	msm_fb_resources[1].start = MSM_OVERLAY_BLT_SIZE;
-	msm_fb_resources[1].end = msm_fb_resources[1].start + size - 1;
-	pr_info("allocating %lu bytes at 0x%p (0x%lx physical) for overlay\n",
-		size, __va(MSM_OVERLAY_BLT_BASE), (unsigned long) msm_fb_resources[1].start);
-#endif
 	persistent_ram_early_init(&ram_console_ram);
 }
 
@@ -3502,7 +3472,6 @@ static struct platform_device pm8058_leds = {
 };
 
 #ifdef CONFIG_FB_MSM_HDMI_MHL
-static int pm8901_mpp_init(void);
 static struct regulator *reg_8901_l0;
 static struct regulator *reg_8058_l19;
 static struct regulator *reg_8901_l3;
@@ -3630,7 +3599,6 @@ static int mhl_sii9234_power(int on)
 		break;
 	case 1:
 		mhl_sii9234_all_power(true);
-		pm8901_mpp_init();
 		config_gpio_table(mhl_gpio_table, ARRAY_SIZE(mhl_gpio_table));
 		break;
 	default:
@@ -3856,11 +3824,17 @@ static void __init reserve_ion_memory(void)
 }
 #endif
 
+static void __init reserve_mdp_memory(void)
+{
+	shooter_mdp_writeback(msm8x60_reserve_table);
+}
+
 static void __init msm8x60_calculate_reserve_sizes(void)
 {
 	size_pmem_devices();
 	reserve_pmem_memory();
 	reserve_ion_memory();
+	reserve_mdp_memory();
 }
 
 static int msm8x60_paddr_to_memtype(phys_addr_t paddr)
@@ -4807,41 +4781,6 @@ static struct spi_board_info msm_spi_board_info[] __initdata = {
 #ifdef CONFIG_PMIC8901
 
 #define PM8901_GPIO_INT           91
-
-#ifdef CONFIG_FB_MSM_HDMI_MHL
-static int pm8901_mpp_init(void)
-{
-    int rc;
-    pr_err("%s\n", __func__);
-
-    rc = pm8901_mpp_config(0, PM_MPP_TYPE_D_BI_DIR,
-		PM8901_MPP_DIG_LEVEL_MSMIO,
-		PM_MPP_BI_PULLUP_10KOHM);
-	if (rc)
-		pr_err("%s: pm8901_mpp_config failed with %d\n", __func__, rc);
-
-	rc = pm8901_mpp_config(1, PM_MPP_TYPE_D_BI_DIR,
-		PM8901_MPP_DIG_LEVEL_L5,
-		PM_MPP_BI_PULLUP_10KOHM);
-	if (rc)
-		pr_err("%s: pm8901_mpp_config failed with %d\n", __func__, rc);
-
-	rc = pm8901_mpp_config(2, PM_MPP_TYPE_D_BI_DIR,
-		PM8901_MPP_DIG_LEVEL_MSMIO,
-		PM_MPP_BI_PULLUP_10KOHM);
-
-	if (rc)
-		pr_err("%s: pm8901_mpp_config failed with %d\n", __func__, rc);
-
-	rc = pm8901_mpp_config(3, PM_MPP_TYPE_D_BI_DIR,
-		PM8901_MPP_DIG_LEVEL_L5,
-		PM_MPP_BI_PULLUP_10KOHM);
-
-	if (rc)
-		pr_err("%s: pm8901_mpp_config failed with %d\n", __func__, rc);
-	return rc;
-}
-#endif
 
 static struct pm8901_gpio_platform_data pm8901_mpp_data = {
 	.gpio_base	= PM8901_GPIO_PM_TO_SYS(0),
@@ -6624,7 +6563,7 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	platform_device_register(&msm_gsbi2_qup_spi_device);
 #endif
 
-	shooter_init_panel(msm_fb_resources, ARRAY_SIZE(msm_fb_resources));
+	shooter_init_fb();
 
 	fixup_i2c_configs();
 	register_i2c_devices();
